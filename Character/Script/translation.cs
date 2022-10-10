311892cac746245ec62daf6977d81f8410561bcb
chelseawww <chelseawu0315@gmail.com>
2022-10-10 16:37:06 +0800

    10/10

--- Character/Script/translation.cs
+++ Character/Script/translation.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class translation : MonoBehaviour
 {
     public float moveSpeed = 1.0f;
+    public float turnSpeed = 0.5f;
 
     // Update is called once per frame
     void Update()
@@ -15,8 +16,8 @@ public class translation : MonoBehaviour
         else if(Input.GetKey("s"))
             transform.Translate(Vector3.forward * -moveSpeed * Time.deltaTime); 
         else if(Input.GetKey("a"))
-            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+            transform.Translate(Vector3.left * turnSpeed * Time.deltaTime);
         else if(Input.GetKey("d"))
-            transform.Translate(Vector3.left * -moveSpeed * Time.deltaTime);
+            transform.Translate(Vector3.left * -turnSpeed * Time.deltaTime);
     }
 }
